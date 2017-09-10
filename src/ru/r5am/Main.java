a62c9185445452411b879a3075f8116e86405b83
zoer <richmanfx@yamdex.ru>
2017-09-10 17:32:04 +0300

    Добавлен библиотека "nproperty" для работы c INI-файлами, реализован класс для чтения INI-файла.

--- src/ru/r5am/Main.java
+++ src/ru/r5am/Main.java
@@ -1,5 +1,9 @@
 package ru.r5am;
 
+/*
+ * Created by Zoer on 09.09.2017.
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,11 +12,21 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    final public static String userHomePath = System.getProperty("user.home");  // папка пользователя
+    final public static String iniFileName = ".tomato/tomato.ini";              // файл конфигурации
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ru.r5am.fxml"));
         primaryStage.setTitle("Помидор");
-        Scene scene = new Scene(root, 300, 275);
+
+        // Установка размеров главной формы
+        primaryStage.setMinWidth(125);
+        primaryStage.setMinHeight(125);
+        primaryStage.setMaxWidth(800);
+        primaryStage.setMaxHeight(600);
+
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         scene.getStylesheets().add(getClass().getResource("tomato.css").toExternalForm());
         primaryStage.show();
---
+++ src/ru/r5am/ReadConfig.java
@@ -0,0 +1,25 @@
+package ru.r5am;
+
+import jfork.nproperty.Cfg;
+import jfork.nproperty.ConfigParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+@Cfg
+public class ReadConfig {
+
+    // Параметры в файле конфигурации с значениями по умолчанию
+    public static String beginTime = "10:00";       // Время запуска Помидора
+    public static int workDuration = 45;            // Длительность работы
+    public static int timeoutDuration = 15;         // Длительность перерыва
+    public static int lunchDuration = 60;           // Длительность обеда
+    public static int untilLunchCycles = 4;         // Количество периодов работа/отдых до обеда
+    public static int afterLunchCycles = 4;         // Количество периодов работа/отдых после обеда
+
+    // Конструктор
+    public ReadConfig() throws NoSuchMethodException, InstantiationException, IllegalAccessException, IOException, InvocationTargetException {
+        ConfigParser.parse(this, Main.userHomePath + File.separator + Main.iniFileName);
+    }
+}
