d200acdda8be563db4f23b3e727249dccba82a59
Aleksandr Yashchuk <Aleksandr.Yashchuk@redsys.ru>
2017-09-13 17:56:34 +0300

    Определение - рабочий ли день в текущий момент.

--- src/ru/r5am/Controller.java
+++ src/ru/r5am/Controller.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXML;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class Controller {
 
@@ -31,14 +35,35 @@ public class Controller {
         if (fullIniFilePath.exists() && fullIniFilePath.isFile()) {
             new ReadConfig();
         } else {
-            ReadConfig.beginTime = "10:00";         // Время запуска Помидора
-            ReadConfig.workDuration = 45;           // Длительность работы
+            ReadConfig.beginTime = "10:00";          // Время запуска Помидора
+            ReadConfig.workDuration = 45;            // Длительность работы
             ReadConfig.timeoutDuration = 15;         // Длительность перерыва
             ReadConfig.lunchDuration = 60;           // Длительность обеда
             ReadConfig.untilLunchCycles = 4;         // Количество периодов работа/отдых до обеда
             ReadConfig.afterLunchCycles = 4;         // Количество периодов работа/отдых после обеда
-        }
 
+        }
+//        System.out.printf("%s", ReadConfig.beginTime);
+
+        // Во время преобразовать
+        SimpleDateFormat beginDataTime = new SimpleDateFormat("yyyy.MM.dd EEEEEEEEEEE HH:mm");
+        System.out.println("Current Date: " + beginDataTime.format(new Date()));
+
+        if (isWorkingDay())
+            System.out.println("Сегодня рабочий день");
 
     }
+
+
+    /**
+     * Определить рабочий ли день в настоящий момент.
+     * @return true - если день рабочий
+     */
+    private boolean isWorkingDay(){
+        SimpleDateFormat dataTime = new SimpleDateFormat("E");
+        String weekDay = dataTime.format(new Date());
+        List<String> workDays = Arrays.asList("Пн", "Вт", "Ср", "Чт", "Пт");
+        return workDays.contains(weekDay);
+    }
+
 }
